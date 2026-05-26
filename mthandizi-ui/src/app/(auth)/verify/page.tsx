91b1754e0d98bf496b7797575c177e13f1cb7b03
BED_COM_24_21 <bed-com-24-21@unima.ac.mw>
2026-05-27 00:32:43 +0200

    verifying the rendering of verify page. Iyi nde ikutheka

--- mthandizi-ui/src/app/(auth)/verify/page.tsx
+++ mthandizi-ui/src/app/(auth)/verify/page.tsx
@@ -1,5 +1,6 @@
 
 import { Suspense } from "react";
+import VerifyContent from "./VerifyContent";
 
 export default function VerifyPage() {
   return (
@@ -11,7 +12,7 @@ export default function VerifyPage() {
         </div>
       </div>
     }>
-
+      <VerifyContent />
     </Suspense>
   );
 }
