9773afa02c9555f2b3a853b4164211b030df2da6
Dumisani Gondwe <dumisanimgondwe@gmail.com>
2026-05-27 12:28:22 +0200

    added malawi data and student application wizard steps

---
+++ mthandizi-ui/src/lib/constants/malawi-data.ts
@@ -0,0 +1,68 @@
+export const MALAWI_DISTRICTS = [
+  "Balaka", "Blantyre", "Chikwawa", "Chiradzulu", "Chitipa",
+  "Dedza", "Dowa", "Karonga", "Kasungu", "Likoma",
+  "Lilongwe", "Machinga", "Mangochi", "Mchinji", "Mulanje",
+  "Mwanza", "Mzimba", "Neno", "Nkhata Bay", "Nkhotakota",
+  "Nsanje", "Ntcheu", "Ntchisi", "Phalombe", "Rumphi",
+  "Salima", "Thyolo", "Zomba",
+];
+
+export const MALAWI_TAS = [
+  // Blantyre
+  "Blantyre", "Kapeni", "Kuntumanji", "Lunzu", "Machinjiri", "Makata", "Matope", "Mpemba", "Nkula",
+  // Lilongwe
+  "Chimutu", "Chitukula", "Chadza", "Kalolo", "Kabudula", "Khongoni", "Linthipe", "Malili", "Mazengera",
+  "Mkukula", "Mwansambo", "Njewa", "Nkukula", "Tsabango",
+  // Zomba
+  "Chikowi", "Chirwa", "Kuntumanji", "Malemia", "Mlumbe", "Mwambo", "Nkanda", "Nsamala", "Zomba",
+  // Mzimba
+  "Chindi", "Emfeni", "Hora", "Kampingo Sibande", "Mzimba", "Mzukuzuku", "Njakwa", "Timbiri",
+  // Kasungu
+  "Chulu", "Kaomba", "Kasungu", "Lukwa", "Mwase", "Njobvu", "Wimbe",
+  // Mangochi
+  "Chimwala", "Jalasi", "Katuli", "Makanjira", "Mangochi", "Nankumba", "Ntonda",
+  // Dedza
+  "Chakhaza", "Dedza", "Kachindamoto", "Kasumbu", "Khwisa", "Tambala",
+  // Salima
+  "Kalonga", "Khombedza", "Kuluunda", "Maganga", "Salima",
+  // Ntcheu
+  "Chikowi", "Kanduku", "Kwataine", "Lizulu", "Ntcheu",
+  // Chiradzulu
+  "Chiradzulu", "Mpama", "Nkalo",
+  // Mulanje
+  "Chikumbu", "Mabuka", "Mlumbe", "Mulanje", "Nkanda",
+  // Thyolo
+  "Bvumbwe", "Chimaliro", "Nansadi", "Thyolo",
+  // Chikwawa
+  "Chapananga", "Chikwawa", "Katunga", "Lundu", "Makhwira", "Ngabu",
+  // Nsanje
+  "Malemia", "Mbenje", "Mlolo", "Nsanje",
+  // Karonga
+  "Karonga", "Kilupula", "Kyungu", "Mwenewenya",
+  // Rumphi
+  "Chikulamayembe", "Mwankhunikira", "Rumphi",
+  // Nkhata Bay
+  "Fukamapiri", "Kabunduli", "Khondowe", "Nkhata Bay",
+  // Nkhotakota
+  "Dwambazi", "Kafuzira", "Mwansambo", "Nkhotakota",
+  // Dowa
+  "Chakhaza", "Dowa", "Kayembe", "Mponela",
+  // Mchinji
+  "Dambe", "Kapelula", "Mchinji", "Mkanda",
+  // Ntchisi
+  "Kalumo", "Malenga Mzoma", "Ntchisi",
+  // Balaka
+  "Balaka", "Nsamala",
+  // Machinga
+  "Chikweo", "Machinga", "Mposa",
+  // Phalombe
+  "Jenala", "Mkhumba", "Phalombe",
+  // Neno
+  "Neno", "Mlauli",
+  // Mwanza
+  "Mwanza", "Neno",
+  // Chitipa
+  "Chitipa", "Kameme", "Mwabulambo", "Nthalire",
+  // Likoma
+  "Likoma",
+].sort();
